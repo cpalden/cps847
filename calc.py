a232f63020b0c0e17e133f92d27cfb9d3d0834fe
cpalden <cpalden@scs.ryerson.ca>
2018-03-03 18:33:17 -0500

    comiited calc.py

--- calc.py
+++ calc.py
@@ -15,5 +15,5 @@ def multiply(x, y):
 def divide(x, y):
     """Divide Function"""
     if y == 0:
-        raise ValueError('Can not divide by zero!')
+        raise ValueError('Cannot divide by zero!')
     return x / y
